36f99a4c246ae31fde9bfbd0982e63834c9e3657
jrwells <jrwells10@gmail.com>
2014-04-12 13:26:48 -0500

    Added method stubs

--- metrics.py
+++ metrics.py
@@ -1,3 +1,18 @@
 class Metrics:
 	@staticmethod
-	
+	def InningRunsTotalRuns(game_data):
+
+	@staticmethod
+	def WalksAndBalks(game_data):
+
+	@staticmethod
+	def PitchingChangeDistribution(game_data):
+
+	@staticmethod
+	def GameBattingAvgVsSeason(game_data):
+
+	@staticmethod
+	def LeadChanges(game_data):
+
+	@staticmethod
+	def RBIDistribution(game_data):
