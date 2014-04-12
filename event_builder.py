0a9b3ed1dc7894fdea16f8315a24304d5d7b46c4
John Brophy <jbrophy@wustl.edu>
2014-04-12 17:45:58 -0500

    made event builder a class

--- event_builder.py
+++ event_builder.py
@@ -1,38 +1,45 @@
 #Creates the events based on data from metrics
 
+#Arbitrary Constants
+INNING_RUN_PERCENT_THRESHOLD = .4
+
 from metrics import *
+class Event_builder:
+	def build_events(self, gameData):
+		if (int(gameData['linescore']['r']['home']) > int(gameData['linescore']['r']['away']) ):
+			self.winning_team = "home"
+		else:
+			self.winning_team = "away"
 
-def build_events(gameData):
-	if (int(gameData['linescore']['r']['home']) > int(gameData['linescore']['r']['away']) ):
-		winning_team = "home"
-	else:
-		winning_team = "away"
+		self.gameData = gameData
 
-	#inning runs total runs
-	inning_runs = Metrics.InningRunsTotalRuns(gameData)
+		#inning runs total runs
+		inning_runs = Metrics.InningRunsTotalRuns(gameData)
 
-	#walked in runs
-	walked_runs = Metrics.WalksAndBalks(gameData)
+		#walked in runs
+		walked_runs = Metrics.WalksAndBalks(gameData)
 
-	#pitching changes
-	pitching_changes = Metrics.PitchingChangeDistribution(gameData)
+		#pitching changes
+		pitching_changes = Metrics.PitchingChangeDistribution(gameData)
 
-	#Game batting ave
-	game_batting_ave = Metrics.GameBattingAvgVsSeason(gameData)
+		#Game batting ave
+		game_batting_ave = Metrics.GameBattingAvgVsSeason(gameData)
 
-	#Lead changes
-	lead_changes = Metrics.LeadChanges(gameData)
+		#Lead changes
+		lead_changes = Metrics.LeadChanges(gameData)
 
-	#RBI percentage
-	rbi_percentage = Metrics.RBIDistribution(gameData)
+		#RBI percentage
+		rbi_percentage = Metrics.RBIDistribution(gameData)
 
-def build_inning_events(inning_metrics):
+	def build_inning_events(self, inning_metrics):
+		for team in inning_metrics:
+			if team[0] > INNING_RUN_PERCENT_THRESHOLD and
 
-def build_walks_events(walks_metrics):
+	def build_walks_events(self, walks_metrics):
 
-def build_pitching_change_events(pitching_metrics):
+	def build_pitching_change_events(self, pitching_metrics):
 
-def build_lead_change_events(lead_metrics):
+	def build_lead_change_events(self, lead_metrics):
 
-def build_rbi_events(rbi_metrics):
+	def build_rbi_events(self, rbi_metrics):
 
