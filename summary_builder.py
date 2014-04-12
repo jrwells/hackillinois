67fb1f351043c46f65bca6e6e741dd06ef06b22e
Nathan Germer <germer.n@gmail.com>
2014-04-12 17:13:23 -0500

    <3

--- summary_builder.py
+++ summary_builder.py
@@ -6,6 +6,7 @@ class SummaryBuilder:
   Add an event to the SummaryBuilder.  Arguments:
     description: the string to add to the Summary
     weight: arbitrary measure of importance between 0<=w<=1
+            shametext is negative
     (team_won: did this team win - True or False
         if None, this event doesn't correspond to a team.)
   """
