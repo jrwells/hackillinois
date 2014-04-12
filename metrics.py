9f60b9dad3b4bf914351321f1e7b2d6f1c4e8116
Jeff Stephens <jefftheman45@gmail.com>
2014-04-12 13:56:15 -0500

    style cleanup

--- metrics.py
+++ metrics.py
@@ -15,12 +15,13 @@ class Metrics:
 	@staticmethod
 	def WalksAndBalks(game_data):
 		""" Gets quantity of runs earned not by RBI (walks/balks/errors) """
+
 		runs_scored = game_data['linescore']['r']
 		runs_scored_away = runs_scored[1]
 		runs_scored_home = runs_scored[0]
 
 		for entry in game_data['boxscore']['batting']:
-			if entry['team_flag'] == "home":
+			if entry['team_flag'] == 'home':
 				rbi_home = entry['rbi']
 			else:
 				rbi_away = entry['rbi']
