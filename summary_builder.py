02e83268d26252d7606db11cde30c90a4cd640df
Nathan Germer <germer.n@gmail.com>
2014-04-12 17:57:49 -0500

    summary builder basically done <3

--- summary_builder.py
+++ summary_builder.py
@@ -1,6 +1,12 @@
+import Queue
+
+SHAME_WEIGHT = 0.05
+
+MAX_STRINGS = 6
+
 class SummaryBuilder:
-  def __main__(self):
-    pass
+  def __main__(self, main_description, winning_team, losing_team):
+    self.pq = Queue.PriorityQueue()
 
   """
   Add an event to the SummaryBuilder.  Arguments:
@@ -11,10 +17,23 @@ class SummaryBuilder:
         if None, this event doesn't correspond to a team.)
   """
   def add_event(self,description,weight,team_won=None):
-    pass
+    if team_won == None or team_won:
+      real_weight = weight
+    else:
+      real_weight = weight - SHAME_WEIGHT #apply the shameeeeee
+
+    ## TODO: APPLY SENTIMENT ANALYSIS
+
+    self.pq.put_nowait((1.0-real_weight,description))
 
   """
   Build a summary for this game.
   """
   def build_summary(self):
-    pass
+    count = 0
+    strings = []
+    while count < MAX_STRINGS and not self.pq.Empty():
+      pri,string = self.pq.get_nowait()
+      strings.append(string)
+
+    return " ".join(strings)
