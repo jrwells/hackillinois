ed3909430c41e59345100f12ff150239f2234ba9
John Brophy <jbrophy@wustl.edu>
2014-04-12 03:16:02 -0500

    Added get_winner()

---
+++ summarize.py
@@ -0,0 +1,19 @@
+
+class summarize:
+	
+	def get_winner(gameData):
+		runs_scored = gameData['linescore']['r']
+		if runs_scored['home'] > runs_scored['away']:
+			winner = gameData['home_team_name']
+			winner_score = runs_scored['home']
+			loser = gameData['away_team_name']
+			loser_score = runs_scored['away']
+		else:
+			winner = gameData['away_team_name']
+			winner_score = runs_scored['away']
+			loser = gameData['home_team_name']
+			loser_score = runs_scored['home']
+
+		return "%s defeated %s %d - %d" % (winner, loser, winner_score, loser_score)
+
+
