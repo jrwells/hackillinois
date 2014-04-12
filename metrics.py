54436c7e1b05df9486da3b9a231186bb62d692a3
jrwells <jrwells10@gmail.com>
2014-04-12 15:38:04 -0500

    changed division by 0 check

--- metrics.py
+++ metrics.py
@@ -10,17 +10,19 @@ class Metrics:
 
 		linescore = game_data['boxscore']['linescore']
 		home_runs, away_runs, home_inning_runs, away_inning_runs = float(linescore['home_team_runs']), float(linescore['away_team_runs']), [], []
-		if home_runs == 0:
-			home_runs = 1
-		if away_runs == 0:
-			away_runs = 1
 
 		for inning in linescore['inning_line_score']:
 			if inning['home'] == 'x':
 				home_inning_runs.append(0.0)
 			else:
-				home_inning_runs.append(float(inning['home'])/home_runs)
-			away_inning_runs.append(float(inning['away'])/away_runs)
+				if home_runs == 0:
+					home_inning_runs.append(0.0)
+				else:
+					home_inning_runs.append(float(inning['home'])/home_runs)
+			if away_runs == 0:
+				away_inning_runs.append(0.0)
+			else:
+				away_inning_runs.append(float(inning['away'])/away_runs)
 
 		away_max, home_max = max(away_inning_runs), max(home_inning_runs)
 		away_tuple, home_tuple = (away_max, away_inning_runs.index(away_max) + 1), (home_max, home_inning_runs.index(home_max) + 1)
