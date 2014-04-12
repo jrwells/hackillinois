a22957dcb3d5e4b1c4206d8b082cd89093352cc9
jrwells <jrwells10@gmail.com>
2014-04-12 05:54:50 -0500

    Added get no hitter

--- summarize.py
+++ summarize.py
@@ -1,5 +1,6 @@
 ERROR_INTEREST_THRESHOLD = 3
-ROOT = "/components/game/mlb/year_2013/month_08/day_21/"
+DO_NOT_MENTION_VALUE = 0
+NO_HITTER_VALUE = 10
 
 class Summarize:
 	@staticmethod
@@ -40,6 +41,42 @@ class Summarize:
 			score = max(int(error_count['home']), int(error_count['away']))
 			error_summary_text = error_summary_text + "."
 		else:
-			score = 0
+			score = DO_NOT_MENTION_VALUE
 
 		return (score, error_summary_text)
+
+	@staticmethod
+	def fetch_boxscore(game_data):
+		root_dir = game_data['game_data_directory'] + '/boxscore.json'
+		boxscore = requests.get(root_dir).json()
+		self.boxscore = boxscore['data']['boxscore']
+
+	@staticmethod
+	def get_no_hitter(game_data):
+		no_hitter_text = ""
+		if self.boxscore:
+			for team in self.boxscore['pitching']:
+				if int(team['h']) == 0:
+					pitchers = team['pitcher']
+					if len(pitchers) == 1:
+						if len(no_hitter_text) == 0:
+							no_hitter_text = pitchers[0]['name_display_first_last']
+						else:
+							no_hitter_text = no_hitter_text + " and " pitchers[0]['name_display_first_last']
+					else:
+						if len(no_hitter_text):
+							if team['team_flag'] == 'away':
+								no_hitter_text = self.boxscore['away_fname']
+							else:
+								no_hitter_text = self.boxscore['home_sname']
+						else:
+							if team['team_flag'] == 'away':
+								no_hitter_text = no_hitter_text + " and " + self.boxscore['away_fname']
+							else:
+								no_hitter_text = no_hitter_text + " and " + self.boxscore['home_sname']
+			if len(no_hitter_text) > 0:
+				no_hitter_text += " had no hitter."
+				return (NO_HITTER_VALUE, no_hitter_text
+			else
+				(DO_NOT_MENTION_VALUE, no_hitter_text)
+
