595b39a4128d60ff2562a2b8c9f89c1c3642a76f
jrwells <jrwells10@gmail.com>
2014-04-12 13:41:03 -0500

    Completed InningsRunsTotalRuns

--- metrics.py
+++ metrics.py
@@ -1,6 +1,16 @@
 class Metrics:
 	@staticmethod
 	def InningRunsTotalRuns(game_data):
+		linescore, home_runs, away_runs, inning_runs = game_data['boxscore']['linescore'], float(game_data['boxscore']['home_team_runs']), float(game_data['boxscore']['away_team_runs']), []
+		for inning in linescore['inning_line_score']:
+			ratios = []
+			for key in inning.keys():
+				if key == 'home':
+					ratios[1] = float(inning[key])/home_runs
+				elif key == 'away':
+					ratios[0] = float(inning[key])/away_runs
+			inning_runs.append(ratios)
+		return inning_runs
 
 	@staticmethod
 	def WalksAndBalks(game_data):
