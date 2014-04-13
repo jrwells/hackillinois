acc0d156efcaabdd463b9c7cba8d5ef47983d6f1
Jeff Stephens <jefftheman45@gmail.com>
2014-04-13 02:10:26 -0500

    day parser in production mode, update parser set to saturday the 12th

--- update_parser.py
+++ update_parser.py
@@ -92,6 +92,10 @@ root = 'http://gd2.mlb.com/components/game/mlb/'
 current_date = datetime.datetime.now()
 day, month, year = current_date.day, current_date.month, current_date.year
 
+day = 12
+month = 4
+year = 2014
+
 url = '%syear_%s/month_%02d/day_%02d/master_scoreboard.json' % (root, year, int(month), int(day))
 
 resetDB()
