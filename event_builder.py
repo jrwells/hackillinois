696660fd79870220a772c9817c1ef43df9751fe8
John Brophy <jbrophy@wustl.edu>
2014-04-12 22:55:09 -0500

    fixed the spelling of offense

--- event_builder.py
+++ event_builder.py
@@ -74,7 +74,7 @@ class EventBuilder:
 		for team in team_designation:
 			#Runs the equation to determine the weight, dependant entirely on
 			#constants at the start of the file
-			weight = (float(inning_metrics[team+'_max']) * float(inning_metrics[team+'_inning']) / float(self.gameData['status']['inning']) * min(1, float(inning_metrics[team+'_max']) / 
+			weight = (float(inning_metrics[team+'_max']) * float(inning_metrics[team+'_inning']) / float(self.gameData['status']['inning']) * min(1, float(inning_metrics[team+'_max']) /
 				INNING_RUN_PERCENT_THRESHOLD) * min(1, inning_metrics[team+'_value'] / INNING_RUN_TOTAL_THRESHOLD) * INNING_RUN_MAX_WEIGHT)
 			log("weight: %s" % weight)
 			#If they lose, inflict a weight penalty
@@ -154,7 +154,7 @@ class EventBuilder:
 				if weight > 0:
 					blurb = "were strong at the plate"
 				else:
-					blurb = "had an off night for offence"
+					blurb = "had an off night for offense"
 
 				events.append(Event(blurb, weight, team_names[i], self.winning_team == team_types[i]))
 
