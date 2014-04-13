29ee97811a08f08b4ec46a8b4e1f50ac00750ece
John Brophy <jbrophy@wustl.edu>
2014-04-13 15:50:30 -0500

    Added support for split tweets

--- tweeter.py
+++ tweeter.py
@@ -12,5 +12,23 @@ class Tweeter:
         access_token_secret='OAGVt4Hq6dzX7NbpXaC1jmsnHPsxjvWVp85ydOhK54tHJ')
 
 	def post_summary(self,summary):
-		self.api.PostUpdate(summary)
-		log("Posted summary %s" % summary)
+		if len(summary) > 140:
+			sentences = summary.split('.')
+			summary_1 = ''
+			summary_2 = ''
+			for sentence in sentences:
+				if len(summary_1) + len(sentence) + 5 < 140:
+					summary_1 += sentence + '.'
+				else:
+					summary_1 += ' 1/2'
+					for sent in sentences[sentences.index(sentence):]:
+						summary_2 += sent + '.'
+
+					summary_2 += ' 2/2'
+					break
+			self.api.PostUpdate(summary_1)
+			self.api.PostUpdate(summary_2)
+
+		else:
+			self.api.PostUpdate(summary)
+			log("Posted summary %s" % summary)
