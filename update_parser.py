20b89f370da3b9e5fd0e6b2e78467885b1596146
Jeff Stephens <jefftheman45@gmail.com>
2014-04-15 15:09:19 -0500

    check past 3 days of queue, support game suspended, no hashtags though

--- update_parser.py
+++ update_parser.py
@@ -12,7 +12,7 @@ def resetDB():
 # return a list of game IDs for games that aren't finished
 def getUnfinishedGames():
 	# get games that have started and haven't finished
-	query = "SELECT `game_id` FROM `games` WHERE `start_time` < NOW() AND `start_time` > DATE_SUB(NOW(), INTERVAL 24 HOUR) AND `finished` = 0;"
+	query = "SELECT `game_id` FROM `games` WHERE `start_time` < NOW() AND `start_time` > DATE_SUB(NOW(), INTERVAL 3 DAY) AND `finished` = 0;"
 	cur.execute(query)
 
 	gameQueue = []
@@ -95,20 +95,21 @@ day, month, year = current_date.day, current_date.month, current_date.year
 # month = 4
 # year = 2014
 
-url = '%syear_%s/month_%02d/day_%02d/master_scoreboard.json' % (root, year, int(month), int(day))
-
-
 queue = getUnfinishedGames()
+loops = 0
 
-if queue:
+while queue:
+	url = '%syear_%s/month_%02d/day_%02d/master_scoreboard.json' % (root, year, int(month), int(day))
+	print url
 	master_scoreboard = json.load(urllib2.urlopen(url))
 	# master_scoreboard = json.load(open("master_scoreboard.json"))
 	summary_tweeter = Tweeter()
 
-	loops = 0
-
 	for record in master_scoreboard['data']['games']['game']:
 		if record['id'] in queue:
+			queue.remove(record['id'])
+			found = False
+
 			if record['status']['status'] == "Final":
 				# fetch boxscore and coalesce
 				root_dir = "http://gd2.mlb.com" + record['game_data_directory'] + '/boxscore.json'
@@ -117,10 +118,24 @@ if queue:
 
 				summary, teaser_text = generateSummary(record)
 
+				found = True
+
+			elif record['status']['status'] == "Suspended":
+				inning = int(record['status']['inning'])
+				k = inning%10
+				ordinal_val = "%d%s"%(inning,"tsnrhtdd"[(inning/10%10!=1)*(k<4)*k::4])
+
+				summary = "%s at %s suspended in the %s due to %s." % (record['away_team_name'], record['home_team_name'], ordinal_val, record['status']['reason'].lower())
+				teaser_text = summary
+				found = True
+
+			if found:
 				query = "UPDATE `games` SET `finished` = 1, `full_summary` = \"%s\", `teaser_text` = \"%s\" WHERE `game_id` = '%s' LIMIT 1;" % (summary, teaser_text, record['id'])
 				cur.execute(query)
 				summary_tweeter.post_summary(teaser_text)
 				loops = loops + 1
 
-	if loops > 0:
-		print "Updated %d game(s)" % loops
+	day -= 1
+
+if loops > 0:
+	print "Updated %d game(s)" % loops
